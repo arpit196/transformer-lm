d3febcd0d54734dc412e6009802326b83e1e8bf1
arpit196 <30366755+arpit196@users.noreply.github.com>
2020-05-14 14:47:26 +0530

    Update model.py

--- lm/gpt_2_tf/model.py
+++ lm/gpt_2_tf/model.py
@@ -132,6 +132,7 @@ def attn(co, x, scope, n_state, *, past, hparams):
 
     def multihead_attn(co, q, k, v):
         # q, k, v have shape [batch, heads, sequence, features]
+        print(q)
         w1 = tf.matmul(q, co, transpose_b=True)
         w = tf.matmul(w1, k, transpose_b=True)
         w = w * tf.rsqrt(tf.cast(v.shape[-1].value, w.dtype))
